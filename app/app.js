6dd6302729b65d6951123e7aa5e3f66405d54396
Stefan Penner <stefan.penner@gmail.com>
2017-10-25 14:26:17 -0700

    add fetch

--- app/app.js
+++ app/app.js
@@ -3,6 +3,10 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
+import fetch from 'fetch';
+
+fetch('http://localhost:1111/').catch(e => console.log(e))
+
 const App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
